ccd7a7997531a813b7ce99dfbc25678e52b61802
Nathan Lo <nathanlo99@hotmail.ca>
2022-01-04 21:14:38 -0500

    Decoupled stb_image compilation from image.cpp, improving compile time

--- src/image.cpp
+++ src/image.cpp
@@ -2,13 +2,8 @@
 
 #include "image.hpp"
 #include "colour.hpp"
+#include "stb.hpp"
 #include "util.hpp"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 
 #include <fstream>
 #include <string>
